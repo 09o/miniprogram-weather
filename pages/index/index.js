3b6eb1ef82f85cebac32d622c0f611be08658caf
09o <igsober@gmail.com>
2018-11-04 12:37:18 +0300

    Nov 4

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,16 @@ const weatherColorMap = {
   'snow': '#99E3FF'
 }
 
+const QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+
+const UNPROMPTED = 0  // 未弹窗
+const UNAUTHORIZED = 1  // 已弹窗拒绝
+const AUTHORIZED = 2    // 已弹窗同意
+
+const UNPROMPTED_TIPS = "点击获取当前位置"  // 未弹窗
+const UNAUTHORIZED_TIPS = "点击开启位置权限"  // 已弹窗拒绝
+const AUTHORIZED_TIPS = ""  // 已弹窗同意
+
 Page({
   data: {
     nowTemp: '',
@@ -23,22 +33,50 @@ Page({
     nowWeatherbg: '',
     hourlyWeather: [],
     todayData: '',
-    todayTemp: ''
+    todayTemp: '',
+    city: '广州市',
+    locationAuthTypes: UNPROMPTED,
+    locationTipsText: UNPROMPTED_TIPS
   },
+
+  // 下拉刷新
   onPullDownRefresh(){
     this.getNow(()=>{
       wx.stopPullDownRefresh()
     })
   },
+
   onLoad() {
+    this.qqmapsdk = new QQMapWX({
+      key: 'EAXBZ-33R3X-AA64F-7FIPQ-BY27J-5UF5B'
+    })
     this.getNow()
   },
+
+  // 授权页面返回中教程所采用的方法，注意：需在app.js中添加代码 'APP({})'
+  onShow() {
+    let MyThis = this
+    wx.getSetting({
+      success (res) {
+        let auth = res.authSetting['scope.userLocation']
+        if (auth && MyThis.data.locationAuthTypes !== AUTHORIZED) {
+          // 权限从无到有
+          MyThis.setData({
+            locationAuthType: AUTHORIZED,
+            locationTipsText: AUTHORIZED_TIPS
+          })
+        MyThis.get_location()
+        }
+      }
+    })
+  },
+
   getNow(callback) {
     let MyThis = this
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now', 
       data: {
-        city: '广州市'
+        city: MyThis.data.city
       },
       success (res) {
         let result = res.data.result
@@ -96,10 +134,67 @@ Page({
     })
   },
 
+  // 跳转
   onTapDayWeather() {
     wx.showToast()
     wx.navigateTo({
-      url: '/pages/list/list',
+      url: '/pages/list/list?city=' + this.data.city
+    })
+  },
+
+  // 获取位置
+  onTapLocation() {
+    let MyThis = this
+
+    // 授权页面返回中我所使用的方法
+    // if (MyThis.data.locationAuthTypes == 1) {
+    //   wx.openSetting({
+    //     success (res) {
+    //       let auth = res.authSetting['scope.userLocation']
+    //       if (auth == true) {
+    //         MyThis.setData({
+    //           locationAuthTypes: UNPROMPTED,
+    //           locationTipsText: UNPROMPTED_TIPS
+    //         })
+    //       }
+    //     }
+    //   })
+
+    if (MyThis.data.locationAuthTypes == 1) {
+      wx.openSetting()
+    } else {
+      MyThis.get_location()
+    }
+  },
+
+  get_location() {
+    let MyThis = this
+    wx.getLocation({
+      success: function (res) {
+        MyThis.setData({
+          locationAuthTypes: AUTHORIZED,
+          locationTipsText: AUTHORIZED_TIPS
+        })
+        MyThis.qqmapsdk.reverseGeocoder({
+          location: {
+            latitude: 39.984060,
+            longitude: 116.307520
+          },
+          success: function (res) {
+            let city = res.result.address_component.city
+            MyThis.setData({
+                city: city,
+            })
+            MyThis.getNow()
+          }
+        })
+      },
+      fail:function () {
+        MyThis.setData({
+          locationAuthTypes: UNAUTHORIZED,
+          locationTipsText: UNAUTHORIZED_TIPS
+        })
+      }
     })
   }
 })
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,3 +1,54 @@
-// pages/list/list.js
+const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+
 Page({
+  data: {
+    weekWeather: [],
+    city: '广州市'
+  },
+  onLoad(options) {
+    this.setData({
+      city: options.city
+    })
+    this.getWeekWeather()
+  },
+  onPullDownRefresh() {
+    this.getWeekWeather(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  getWeekWeather(callback) {
+    let MyThis = this
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/future',
+      data: {
+        time: new Date().getTime(),
+        city: MyThis.data.city
+      },
+      success(res) {
+        let result = res.data.result
+        MyThis.setweekWeather(result)
+      },
+      complete() {
+        callback && callback()
+      }
+    })
+  },
+  setweekWeather(result) {
+    let MyThis = this
+    let weekWeather = []
+    for ( let i=0; i<7; i++ ) {
+      let date = new Date()
+      date.setDate(date.getDate()+i) // 未来七天的日期
+      weekWeather.push({
+        day: dayMap[date.getDay()],  // date.getDay() 返回一周中的第几天，0表示星期日
+        date: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`,
+        temp: `${result[i].minTemp}°-${result[i].maxTemp}°`,
+        iconPath: '/images/' + result[i].weather + '-icon.png'
+      })
+    }
+    weekWeather[0].day = '今天'
+    MyThis.setData({
+      weekWeather: weekWeather
+    })
+  }
 })
